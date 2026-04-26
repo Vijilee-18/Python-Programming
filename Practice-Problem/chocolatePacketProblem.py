bd80a74780ed28437edc91d350fe805306222d30
Vijilee George Kurian <vijileegk2006@gmail.com>
2026-04-26 12:30:49 +0530

    Refactor chocolatePacketCalculation for packet sorting
    Refactored chocolatePacketCalculation function to sort chocolate packets, moving empty packets to the end.

--- Practice-Problem/chocolatePacketProblem.py
+++ Practice-Problem/chocolatePacketProblem.py
@@ -1,3 +1,14 @@
+'''
+Description : A chocolate factory is packing chocolates into the packets. The chocolate packets here represent an array  of N number of integer values. 
+              The task is to find the empty packets(0) of chocolate and push it to the end of the array.
+Example :
+         Input :
+                8  – Value of N
+                [4,5,0,1,9,0,5,0] – Element of arr[O] to arr[N-1],While input each element is separated by newline
+         Output :
+                 4 5 1 9 5 0 0 0
+'''
+
 def chocolatePacketCalculation(noPacket):
     packetList=[]
 
