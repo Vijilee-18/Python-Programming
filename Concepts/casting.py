eda60cbbec51b468877ef8e9fa14c62362843ea5
Vijilee George Kurian <vijileegk2006@gmail.com>
2026-05-30 11:24:41 +0530

    Add files via upload

---
+++ Concepts/casting.py
@@ -0,0 +1,22 @@
+'''
+Some Times we have to specify a type on to a variable. This can be done with casting. 
+Python is an object-orientated language, and as such it uses classes to define data types, including its primitive types.'''
+
+# To Int Type Caster
+y=int(2.3)
+z=int('28')
+print(y,z)
+
+#To Float Type Caster 
+a=float(1) 
+b=float('3')
+print(a,b)
+
+#To Complex Type Caster
+e=complex(2.9)
+print(e)
+
+#To String
+p=str(1)
+q=str(3.8)
+print(p,q)
