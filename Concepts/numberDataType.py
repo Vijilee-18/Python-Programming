b5e1f1a2357d001bd26de045fb1025059acb8da5
Vijilee George Kurian <vijileegk2006@gmail.com>
2026-05-30 11:04:21 +0530

    Add files via upload

---
+++ Concepts/numberDataType.py
@@ -0,0 +1,19 @@
+'''
+In Python Data Type of a Variable id determined based on the value it has.
+In Python The Numeric Value Of a Variable can be Integer , Floating point , Complex Numbers .
+'''
+#Integer Value Can be positive or Negative and can have any length.
+integerVar1=12
+integerVar2=-24
+integerVar3=123456789
+
+#Floating Point Value Can also be Positive or Negative .
+floatingVar1=1.0
+floatingVar2=-1.0
+
+#Complex Value like a+bj can also be represented . where a is the real part and b is the imaginary part . In python we Use j not i in the imaginary part
+complexVar=3+5j
+print(complexVar.real) #gives the real part 3.0
+print(complexVar.imag) #gives the Complex part 5.0
+
+#Conversion of Complex Type To any other is not possible . whereas all other Can.
