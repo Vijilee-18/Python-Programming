d561b4576ff5551903316017b69fa7818acc094a
Vijilee George Kurian <vijileegk2006@gmail.com>
2026-06-06 08:56:09 +0530

    Add files via upload

---
+++ Concepts/match.py
@@ -0,0 +1,34 @@
+# '''
+# The match statement is used to perform different actions based on different conditions.
+# SYNTAX
+# match expression:
+#   case x:
+#     code block
+#   case y:
+#     code block
+#   case z:
+#     code block
+#   case _:
+#     code block
+# '''
+# day=5
+# match day:
+#     case 1:
+#         print("Sunday")
+#     case 2:
+#         print("Monday")
+#     case 3:
+#         print("Tuesday")
+#     case 4:
+#         print("Wednesday")
+#     case 5:
+#         print("Thursday") 
+#     case 6:
+#         print("Friday")
+#     case 7:
+#         print("Saturday")   
+#     case _:
+#         print("Invalid Option")
+x=range(1,10)
+y=list(x)
+print(y)
